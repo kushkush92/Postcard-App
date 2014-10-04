9826c94f27a8459986b29d5b542c4f1758c7e4fd
kushkush92 <khushimahmood@live.co.uk>
2014-10-04 19:55:03 +0100

    first rev of project

---
+++ Postcard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Postcard
+//
+//  Created by Khushi Mahmood on 02/10/2014.
+//  Copyright (c) 2014 Khushi Mahmood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Postcard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Postcard
+//
+//  Created by Khushi Mahmood on 02/10/2014.
+//  Copyright (c) 2014 Khushi Mahmood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
